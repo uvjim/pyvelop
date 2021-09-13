9b8ee370a7ff8ac3dd23314c68c568ee6fbb080c
uvjim <uvjim@users.noreply.github.com>
2021-09-13 19:13:56 +0100

    Tidy up the file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,20 +1,5 @@
+# noinspection PyUnresolvedReferences
 import sphinx_rtd_theme
-
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
 # -- Imports for package constants -------------------------------------------
 # noinspection PyProtectedMember
@@ -23,18 +8,14 @@ from pyvelop.const import (
     _PACKAGE_NAME,
 )
 
-# -- Project information -----------------------------------------------------
 
+# -- Project information -----------------------------------------------------
 project = _PACKAGE_NAME
 copyright = f"2021, {_PACKAGE_AUTHOR}"
 author = _PACKAGE_AUTHOR
 
 
 # -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx_rtd_theme",
@@ -43,22 +24,8 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "sphinx_rtd_theme"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
