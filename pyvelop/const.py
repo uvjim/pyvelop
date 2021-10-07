e39df2176a27ff1f43848a164a81bc9d0ea92755
uvjim <uvjim@users.noreply.github.com>
2021-10-07 19:41:57 +0100

    Bump version

--- pyvelop/const.py
+++ pyvelop/const.py
@@ -5,7 +5,7 @@ from typing import List
 # region #-- generic details --#
 _PACKAGE_NAME = "pyvelop"
 _PACKAGE_AUTHOR = "uvjim"
-_PACKAGE_VERSION = "2021.10.4"
+_PACKAGE_VERSION = "2021.10.5"
 # endregion
 
 # region #-- JNAP actions --#
