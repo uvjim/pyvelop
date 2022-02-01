ca58b8baa81b3706b30496606e27e06807c1f589
uvjim <uvjim@users.noreply.github.com>
2022-02-01 09:19:11 +0000

    Bump version

--- pyvelop/const.py
+++ pyvelop/const.py
@@ -5,7 +5,7 @@ from typing import List
 # region #-- generic details --#
 _PACKAGE_NAME = "pyvelop"
 _PACKAGE_AUTHOR = "uvjim"
-_PACKAGE_VERSION = "2022.1.3"
+_PACKAGE_VERSION = "2022.1.4"
 # endregion
 
 # region #-- JNAP actions --#
