97f115fea66912e8985f2055d95e67c63bb6563f
uvjim <uvjim@users.noreply.github.com>
2022-09-18 14:14:14 +0100

    bump version

--- pyvelop/const.py
+++ pyvelop/const.py
@@ -3,7 +3,7 @@
 # region #-- generic details --#
 _PACKAGE_NAME: str = "pyvelop"
 _PACKAGE_AUTHOR: str = "uvjim"
-_PACKAGE_VERSION: str = "2022.9.5"
+_PACKAGE_VERSION: str = "2022.9.6"
 # endregion
 
 DEF_REDACT: str = "**REDACTED**"
