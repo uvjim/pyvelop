48cf11df3a5fa54df1cfec82061e015103d4dfa1
uvjim <git@simpsoncomputers.co.uk>
2022-10-05 20:16:28 +0100

    bump version

--- pyvelop/const.py
+++ pyvelop/const.py
@@ -3,7 +3,7 @@
 # region #-- generic details --#
 _PACKAGE_NAME: str = "pyvelop"
 _PACKAGE_AUTHOR: str = "uvjim"
-_PACKAGE_VERSION: str = "2022.9.6"
+_PACKAGE_VERSION: str = "2022.10.1"
 # endregion
 
 DEF_REDACT: str = "**REDACTED**"
