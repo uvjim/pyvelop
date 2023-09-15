fefea5c2e45bef5d138d2fb4b9df7d89de866db3
uvjim <git@uvjim.co.uk>
2023-09-15 21:13:34 +0100

    bump version

--- pyvelop/const.py
+++ pyvelop/const.py
@@ -3,7 +3,7 @@
 # region #-- generic details --#
 _PACKAGE_NAME: str = "pyvelop"
 _PACKAGE_AUTHOR: str = "uvjim"
-_PACKAGE_VERSION: str = "2023.9.1b0"
+_PACKAGE_VERSION: str = "2023.9.1"
 # endregion
 
 DEF_REDACT: str = "**REDACTED**"
